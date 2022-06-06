f1c63d72cb006c2cb3aad20c2a9746d3961ec4af
vinaamer <104452154+vinaamer@users.noreply.github.com>
2022-06-06 09:27:50 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,37 @@
-#!/bin/bash
-sudo apt install bc
-sudo apt install gcc-arm-linux-gnueabi
-sudo apt install gcc-aarch64-linux-gnu
-export KBUILD_BUILD_USER=Olympus
-export KBUILD_BUILD_HOST=zeus-host
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone https://github.com/mvaisakh/gcc-arm64 --depth=1
+git clone https://github.com/mvaisakh/gcc-arm --depth=1
+git clone --depth=1 https://github.com/vinaamer/olympus.git -b Olympus AnyKernel
+echo "Done"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+export CONFIG_PATH=$PWD/arch/arm64/configs/RMX1801_defconfig
+PATH="$(pwd)/gcc-arm64/bin:$(pwd)/gcc-arm/bin:${PATH}" \
+export ARCH=arm64
+export USE_CCACHE=1
+export KBUILD_BUILD_HOST=God-Of-Olympus
+export KBUILD_BUILD_USER="Zeus"
+
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 RMX1801_defconfig
-    make -j$(nproc) ARCH=arm64 O=out \
-                               CROSS_COMPILE=aarch64-linux-gnu- \
-                               CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+   make O=out ARCH=arm64 RMX1801_defconfig
+     PATH="$(pwd)/gcc-arm64/bin:$(pwd)/gcc-arm/bin:${PATH}" \
+       make -j$(nproc --all) O=out \
+                             ARCH=arm64 \
+                             CROSS_COMPILE=aarch64-elf- \
+                             CROSS_COMPILE_ARM32=arm-eabi-
+   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+   zip -r9 Avalanche-RMX1801-EAS-${TANGGAL}.zip *
+    curl https://bashupload.com/Olympus${TANGGAL}.zip --data-binary @Olympus${TANGGAL}.zip
+    cd ..
 }
 compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
